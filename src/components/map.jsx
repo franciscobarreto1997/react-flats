22104b1bd41ece678f70092cefae29012510d398
Francisco Barreto <franciscoantunesbarreto1997@gmail.com>
2019-12-18 18:13:24 +0000

    hidden api key

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -5,11 +5,12 @@ import Marker from './marker.jsx';
 
 class SimpleMap extends Component {
   render() {
+    {console.log(process.env.REACT_APP_GOOGLE_API_KEY)}
     return (
       // Important! Always set the container height explicitly
       <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: "AIzaSyAAtmb9QKTFebq50ULLhjZdJBiw-ENvBwo" }}
+          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
